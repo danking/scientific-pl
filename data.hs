0981b5620ffd359dbc3536b98f973ed4878a8f79
Daniel King <daniel.zidan.king@gmail.com>
2014-05-23 13:05:50 -0400

    Add data definitions

---
+++ data.hs
@@ -0,0 +1,52 @@
+module SPLData where
+
+type TypeId = String
+
+data Type = IntegerT
+          | ProductT Type Type
+          | UnitT
+          | SumT Type Type
+          | BottomT
+          | ArrayT Integer Type
+          | FunctionT [Type] Type
+          | DistributionT Type
+          | TypeVar TypeId
+          deriving (Eq, Show, Read)
+
+type TypeConstraintID = String
+
+type TypeConstraint = (TypeConstraintID, Type)
+
+data TypeScheme = QuantifiedConstraintsTS [TypeId] [TypeConstraint] Type
+                deriving (Eq, Show, Read)
+
+type Id = String
+
+data Expr = VarE Id
+          | TypeInstantiateE Expr [Type] [Expr]
+          | AppE Expr [Expr]
+          | MatchE Expr [(Pattern, Expr)]
+          | SubscriptE Expr Expr
+          | SubscriptUpdateE Expr Expr Expr
+          | SequenceE Expr Expr
+            -- base types
+          | IntegerE Integer
+          | PrimOpE Id Expr Expr
+          | PairE Expr Expr
+          | UnitE
+          | InLeftE Expr
+          | InRightE Expr
+          deriving (Eq, Show, Read)
+
+data Pattern = PatternVarP Id Type
+             | PatternTupleP Pattern Pattern
+             | PatternUnitP
+             | PatternInLeftP Pattern
+             | PatternInRightP Pattern
+             deriving (Eq, Show, Read)
+
+data Statement = Expr
+               | FunctionDefinitionS Id [Id] TypeScheme Expr
+               | ClassS TypeConstraintID TypeId [TypeConstraint] [(Id, Type)]
+               | InstanceS Id TypeConstraintID TypeId [(Id, Expr)]
+               deriving (Eq, Show, Read)
